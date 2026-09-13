b52ed13c4a6f97e0e5136d1a8ca7bd5a7b28c3fc
Anton Norman <normananton03@gmail.com>
2026-09-13 19:27:54 +0200

    feat(static-config): Adding static config object

---
+++ src/seriousdb/configuration.py
@@ -0,0 +1,54 @@
+
+
+from dataclasses import dataclass, fields
+import json
+import os
+from typing import Any
+import warnings
+
+def get_configuration(file_path: str = "config.json") -> Configuration:
+    if not os.path.isfile(file_path):
+        return Configuration()
+    with open(file_path, "r") as config_file:
+        json_data = json.loads(config_file.read())
+    config = Configuration.from_json(json_data)
+
+    return config
+
+@dataclass
+class Configuration:
+    port: int = 8000
+    debug: bool = True
+    require_auth: bool = False
+    db_file: str = ".sdb"
+
+    @staticmethod
+    def warning_message() -> str:
+        message = "\nConfiguration-file is malformed. The supported fields are:"
+        message += "\n\tport: int"
+        message += "\n\tdebug: bool"
+        message += "\n\trequire_auth: bool"
+        message += "\n\tdb_file: str"
+        return message
+
+    @classmethod
+    def from_json(cls, json_data: dict[str, Any]) -> Configuration:
+        try:
+            conf = cls(**json_data)
+            return conf
+        except:
+            warnings.warn(Configuration.warning_message(), MalformedConfigurationWarning)
+        known_fields = {f.name: f for f in fields(cls)}
+
+        valid_kwargs = {}
+        for name, field in known_fields.items():
+            if name in json_data:
+                val = json_data[name]
+                if field.type is int and isinstance(val, bool):
+                    continue
+                elif isinstance(val, field.type):
+                    valid_kwargs[name] = val
+        return cls(**valid_kwargs)
+
+class MalformedConfigurationWarning(UserWarning):
+    pass
---
+++ tests/test_config.py
@@ -0,0 +1,75 @@
+import pytest
+from pathlib import Path
+from seriousdb.configuration import Configuration, MalformedConfigurationWarning, get_configuration
+
+
+def test_default():
+    has = get_configuration(file_path="this.file.does.not.exits")
+    want = Configuration()
+    assert has == want
+
+def test_malformed():
+    test_config = {
+            "port": 8888,
+            "debug": False,
+            "is_malformed": True,
+            "db_file": "test.sdb"
+            }
+    with pytest.warns(MalformedConfigurationWarning):
+        has = Configuration.from_json(test_config)
+    assert has.port == 8888
+    assert has.debug == False
+    assert has.require_auth == False
+    assert has.db_file == "test.sdb"
+
+def test_correct_formated():
+    test_config = {
+            "port": 9999,
+            "debug": False,
+            "require_auth": True,
+            "db_file": "test.sdb"
+            }
+    has = Configuration.from_json(test_config)
+    assert has.port == 9999
+    assert has.debug == False
+    assert has.require_auth == True
+    assert has.db_file == "test.sdb"
+
+def test_correct_with_one_missing_field():
+    test_config = {
+            "debug": False,
+            "require_auth": True,
+            "db_file": "test.sdb"
+            }
+    has = Configuration.from_json(test_config)
+    assert has.port == 8000
+    assert has.debug == False
+    assert has.require_auth == True
+    assert has.db_file == "test.sdb"
+
+
+def test_correct_with_mutliple_missing_fields():
+    test_config = {
+            "port": 9090
+            }
+    has = Configuration.from_json(test_config)
+    assert has.port == 9090
+    assert has.debug == True
+    assert has.require_auth == False
+    assert has.db_file == ".sdb"
+
+
+def test_correct_with_empty_config():
+    test_config = dict()
+    has = Configuration.from_json(test_config)
+    assert has.port == 8000
+    assert has.debug == True
+    assert has.require_auth == False
+    assert has.db_file == ".sdb"
+
+def test_partial_file_config():
+    has = get_configuration("./tests/test.config.json")
+    assert has.port == 9999
+    assert has.debug == True
+    assert has.require_auth == False
+    assert has.db_file == ".sdb"
