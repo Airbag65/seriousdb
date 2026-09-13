293ee8a3dba751a0cacc8648ccbfc5f81740fd22
Leonardo <leonardo.estacio.h@gmail.com>
2026-09-13 17:51:43 -0500

    fix: run handlers with the thread pool

--- src/seriousdb/main.py
+++ src/seriousdb/main.py
@@ -24,7 +24,7 @@ def get_cache() -> Cache:
 
 
 @app.put("/db")
-async def put(
+def put(
     key: str,
     value: str,
     background_tasks: BackgroundTasks,
@@ -36,5 +36,5 @@ async def put(
 
 
 @app.get("/db")
-async def get(key: str, cache: Annotated[Cache, Depends(get_cache)]):
+def get(key: str, cache: Annotated[Cache, Depends(get_cache)]):
     return select(key, cache)
